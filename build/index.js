3ba767e36fea74fd843e089b64493c788c9b4ae9
jlarosa-utn <jlarosa@est.utn.frba.edu.ar>
2020-12-25 15:28:50 -0300

    create proxy server

--- build/index.js
+++ build/index.js
@@ -1,2 +1,40 @@
 "use strict";
-console.log('Hello world!');
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var http = __importStar(require("http"));
+var http_proxy_1 = __importDefault(require("http-proxy"));
+//
+// Create your proxy server and set the target in the options.
+//
+var PORT = parseInt(process.env.PORT || "8000");
+var URL_PROXY = process.env.URL_PROXY || "http://localhost:9000";
+http_proxy_1.default.createProxyServer({ target: URL_PROXY }).listen(PORT); // See (†)
+//
+// Create your target server
+//
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers));
+    res.end();
+}).listen(9000);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello world!')
+import * as http from 'http'
+import Server from 'http-proxy'
+//
+// Create your proxy server and set the target in the options.
+//
+const PORT = parseInt(process.env.PORT || "8000");
+const URL_PROXY = process.env.URL_PROXY || `http://localhost:9000` 
+Server.createProxyServer({target: URL_PROXY}).listen(PORT); // See (†)
+ 
+//
+// Create your target server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers));
+  res.end();
+}).listen(9000);
